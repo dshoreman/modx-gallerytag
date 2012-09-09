96dd243b6338f74c8cc0f7faf6fccbda2ecbc83b
Dave Shoreman <codeM0nK3Y@me.com>
2012-09-09 12:52:14 +0100

    Finally got some output from the DB

--- core/components/gallerytag/elements/snippets/snippet.galleryTag.php
+++ core/components/gallerytag/elements/snippets/snippet.galleryTag.php
@@ -7,15 +7,60 @@
  * @author Dave Shoreman <codeM0nK3Y@me.com>
  */
 
-$tag_q = "SELECT DISTINCT t.tag FROM gallery_album_items AS a LEFT JOIN gallery_tags AS t ON t.item = a.item WHERE a.album = '{$albumId}'";
-$result = $modx->db->query($tag_q);
+$rowboat = $modx->getService('rowboat','Rowboat',$modx->getOption('rowboat.core_path',null,$modx->getOption('core_path').'components/rowboat/').'model/rowboat/',$scriptProperties);
+if (!($rowboat instanceof Rowboat)) return 'no rowboat';
 
-$tags = '';
-while ($tag = $modx->db->getRow($result))
-{
-	$tags .= $modx->getChunk($innerTpl, array(
-		'tag' => $tag['tag']
-	));
+$columns = $modx->getOption('columns',$scriptProperties,'');
+$limit = $modx->getOption('limit',$_REQUEST,$modx->getOption('limit',$scriptProperties,10));
+$offset = $modx->getOption('offset',$_REQUEST,$modx->getOption('offset',$scriptProperties,0));
+
+$c = $rowboat->newQuery('gallery_tags');
+if (empty($c)) {
+    return $modx->lexicon('rowboat.no_driver',array('dbtype' => $modx->config['dbtype']));
+}
+if ($columns != '*') {
+    $columns = $modx->fromJSON($columns);
+    if (!empty($columns)) {
+        $c->select($columns);
+    }
+}
+if (!empty($where)) {
+    $where = $modx->fromJSON($where);
+    if (!empty($where)) {
+        $c->where($where);
+    }
 }
 
-return $modx->getChunk($outerTpl, array('tags' => $tags));
+$cc = null;
+if (intval($limit) > 0) {
+    /** @var rbQuery $cc */
+    $cc = clone $c;
+    $c->limit($limit,$offset);
+}
+$sql = $c->toSql();
+
+if ($c->execute()) {
+    $results = $c->getResults();
+    if (!empty($cc)) {
+        $total = $cc->count();
+    } else {
+        $total = count($results);
+    }
+    $c->close();
+}
+
+/* iterate across results */
+$tags = '';
+if (is_array($results)) {
+    foreach ($results as $row)
+    {
+        $tags .= $rowboat->getChunk($innerTpl, $row);
+    }
+}
+
+/* output */
+$output = $modx->getChunk($outerTpl, array('tags' => $tags));
+
+return $output;
+
+//$tag_q = "SELECT DISTINCT t.tag FROM gallery_album_items AS a LEFT JOIN gallery_tags AS t ON t.item = a.item WHERE a.album = '{$albumId}'";
