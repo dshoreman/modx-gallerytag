92a60803f926d8ef2bc3cca2ffb5df0da631f142
Dave Shoreman <codeM0nK3Y@me.com>
2012-09-09 13:42:28 +0100

    Whitespacecorrected

--- core/components/gallerytag/elements/snippets/snippet.galleryTag.php
+++ core/components/gallerytag/elements/snippets/snippet.galleryTag.php
@@ -20,36 +20,36 @@ $where = array('a.album' => $albumId);
 
 $c = $rowboat->newQuery($table);
 if (empty($c)) {
-    return $modx->lexicon('rowboat.no_driver',array('dbtype' => $modx->config['dbtype']));
+	return $modx->lexicon('rowboat.no_driver',array('dbtype' => $modx->config['dbtype']));
 }
 
 if ($columns != '*') {
-    if (!empty($columns)) {
-        $c->select($columns);
-    }
+	if (!empty($columns)) {
+		$c->select($columns);
+	}
 }
 
 if (!empty($where)) {
-    if (!empty($where)) {
-        $c->where($where);
-    }
+	if (!empty($where)) {
+		$c->where($where);
+	}
 }
 
 $sql = $c->toSql();
 var_dump($sql);
 if ($c->execute()) {
-    $results = $c->getResults();
-    $total = count($results);
-    $c->close();
+	$results = $c->getResults();
+	$total = count($results);
+	$c->close();
 }
 
 /* iterate across results */
 $tags = '';
 if (is_array($results)) {
-    foreach ($results as $row)
-    {
-        $tags .= '<pre>'.print_r($row, true).'</pre>';//$rowboat->getChunk($innerTpl, $row);
-    }
+	foreach ($results as $row)
+	{
+		$tags .= '<pre>'.print_r($row, true).'</pre>';//$rowboat->getChunk($innerTpl, $row);
+	}
 }
 
 return $modx->getChunk($outerTpl, array('tags' => $tags));
